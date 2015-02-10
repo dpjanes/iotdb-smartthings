bb30c56a6618520e6d0a85534f0b53e2e2c17bf6
David Janes <davidjanes@davidjanes.com>
2015-02-10 18:47:38 -0500

    distributed

---
+++ DIST.sh
@@ -0,0 +1,45 @@
+#
+#   DIST.sh
+#
+#   David Janes
+#   IOTDB
+#   2015-02-10
+#
+
+PACKAGE=iotdb-smartthings
+DIST_ROOT=/var/tmp/.dist.$$
+
+if [ ! -d "$DIST_ROOT" ]
+then
+    mkdir "$DIST_ROOT"
+fi
+
+echo "=================="
+echo "NPM Packge: $PACKAGE"
+echo "=================="
+(
+    NPM_DST="$DIST_ROOT/$PACKAGE"
+    echo "NPM_DST=$NPM_DST"
+
+    if [ -d ${NPM_DST} ]
+    then
+        rm -rf "${NPM_DST}"
+    fi
+    mkdir "${NPM_DST}" || exit 1
+
+    update-package || exit 1
+
+    tar cf - \
+        README.md \
+        LICENSE.txt \
+        package.json smartthings.js smartthingslib.js \
+        |
+    ( cd "${NPM_DST}" && tar xvf - )
+
+    ## cp dist/*.* "${NPM_DST}" || exit 1
+
+    cd "${NPM_DST}" || exit 1
+    npm publish
+
+    echo "end"
+)
