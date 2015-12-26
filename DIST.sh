7d43f4140bc53e744a949ed119631317a27836ec
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:49 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -34,12 +34,9 @@ echo "=================="
         LICENSE.txt \
         package.json smartthings.js smartthingslib.js \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    ## cp dist/*.* "${NPM_DST}" || exit 1
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
